5ba83d2519ce4b3de5ea35aef131bae3ba8ad911
Igor Timofeev <timgarik@mail.ru>
2025-07-02 11:21:09 +0300

    fix136

--- ads.js
+++ ads.js
@@ -45,10 +45,10 @@
                 appId: "2700",
             });
 
-            richBannerDiv = document.createElement("div");
-            richBannerDiv.setAttribute("rich-banner", "365397");
-            richBannerDiv.style.display = "none";
-            document.body.appendChild(richBannerDiv);
+            richBannerDiv = document.querySelectorAll('#rich-banner-365397');
+            if (richBannerDiv) {
+                richBannerDiv.style.display = "none";
+            }
 
             const adsNotFoundCallback = () => {
                 console.log('No ads found to show');
